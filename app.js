7861985cb4f7d189ad1c102c9c48c2e3b868450d
Alisson Souza <souzaa47@yahoo.com>
2021-07-18 08:10:05 -0300

    Alteração no Layout

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.static("public"));
 
-app.get('/', (req, res) => res.send("Bem vindo"));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')});
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')});
+
+
 app.get('/manutencao', (req, res) => {
     res.sendFile(__dirname + '/views/manutencao.html')});
 
+
+
 app.listen(3000, () => console.log("Servidor foi iniciado!"));
+
